2a5f8c7486715ff22841677bd046d02c30c687ca
RJM <rjmbowie@gmail.com>
2022-08-01 21:16:58 +1000

    fix: Authenticate with gh

--- build_neovim.sh
+++ build_neovim.sh
@@ -1,13 +1,14 @@
+mkdir vendor
+git clone https://github.com/neovim/neovim vendor/neovim
+cd vendor/neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
+cd ../../
 while true
-  read -l -P 'Do you want to continue? [y/N] ' yn
+  read -l -P 'Do you want to remove the cloned neovim repository? [y/N] ' yn
   switch $yn
     case Y y
-      rm -rf neovim/
+      rm -rf vendor/neovim/
     case '' N n
       break
   end
 end
-# git clone https://github.com/neovim/neovim
-
-# cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-# sudo make install
