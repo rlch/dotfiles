a8f6f91ff2dccd51690784bbfb9ffb293cf10327
RJM <rjmbowie@gmail.com>
2023-06-07 12:00:29 +1000

    fix: Misc changes

--- config/nvim/lua/rm/plugins/init.lua
+++ config/nvim/lua/rm/plugins/init.lua
@@ -51,7 +51,7 @@ return packer.startup {
         config = module "treesitter",
         requires = {
           "JoosepAlviste/nvim-ts-context-commentstring",
-        }
+        },
       },
       "nvim-treesitter/playground",
     }
@@ -415,6 +415,30 @@ return packer.startup {
             show_current_context = true,
             show_current_context_start = false,
             filetype = { "yaml", "gotmpl", "yml", "pubspec" },
+          }
+        end,
+      },
+      {
+        "levouh/tint.nvim",
+        config = function()
+          require("tint").setup {
+            tint = -2,
+            saturation = 0.9,
+            -- transforms = require("tint").transforms.SATURATE_TINT,
+            tint_background_colors = true,
+            highlight_ignore_patterns = {
+              "WinSeparator",
+              "Status.*",
+            },
+            window_ignore_function = function(winid)
+              local bufid = vim.api.nvim_win_get_buf(winid)
+              ---@diagnostic disable-next-line: redundant-parameter
+              local buftype = vim.api.nvim_buf_get_option(bufid, "buftype")
+              local floating = vim.api.nvim_win_get_config(winid).relative ~= ""
+
+              -- Do not tint `terminal` or floating windows, tint everything else
+              return buftype == "terminal" or floating
+            end,
           }
         end,
       },
---
+++ config/nvim/luasnippets/json.lua
@@ -0,0 +1,33 @@
+---@diagnostic disable: undefined-global
+return {
+  snippet(
+    "avro-record",
+    fmt(
+      [[
+      {
+        "type": "record",
+        "name": "<name>",
+        "namespace": "<namespace>",
+        "fields": [
+          <fields>
+        ]
+      }
+      ]],
+      {
+        name = i(1, "name"),
+        namespace = d(2, function()
+          local repo_path = vim.fn.system {
+            "git",
+            "rev-parse",
+            "--show-toplevel",
+          }
+          local repo_name = repo_path:gsub("^%s*(.*/)([^%s]*)%s*$", "%2")
+          repo_name = repo_name:gsub("-", "")
+          return sn(nil, { t(repo_name) })
+        end),
+        fields = i(3, ""),
+      },
+      { delimiters = "<>" }
+    )
+  ),
+}
