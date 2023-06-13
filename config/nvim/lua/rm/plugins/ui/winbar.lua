d998beaf7e2dc22076adf90c90683e17dc3477d7
RJM <rjmbowie@gmail.com>
2023-06-13 11:31:35 +1000

    fix: Update from desktop

--- config/nvim/lua/rm/plugins/ui/winbar.lua
+++ config/nvim/lua/rm/plugins/ui/winbar.lua
@@ -1,25 +1,5 @@
 ---@class dropbar_configs_t
 local opts = {
-  general = {
-    ---@type boolean|fun(buf: integer, win: integer): boolean
-    enable = function(buf, win)
-      return not vim.api.nvim_win_get_config(win).zindex
-        and vim.bo[buf].buftype == ""
-        and vim.api.nvim_buf_get_name(buf) ~= ""
-        and not vim.wo[win].diff
-    end,
-    update_events = {
-      "CursorMoved",
-      "CursorMovedI",
-      "DirChanged",
-      "FileChangedShellPost",
-      "TextChanged",
-      "TextChangedI",
-      "VimResized",
-      "WinResized",
-      "WinScrolled",
-    },
-  },
   icons = {
     kinds = {
       use_devicons = true,
