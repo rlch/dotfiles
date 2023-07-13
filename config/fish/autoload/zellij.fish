1d33ab902880a4d6598aae1568b1354e475cd0f7
RJM <rjmbowie@gmail.com>
2023-07-13 23:28:13 +1000

    fix: Autoload tabs for any repo

--- config/fish/autoload/zellij.fish
+++ config/fish/autoload/zellij.fish
@@ -1,15 +1,22 @@
 function zl
-  if not test -f layout.kdl
+  set NAME (basename "$PWD")
+  set LAYOUT_NAME ""
+  if test -f layout.kdl
+    set LAYOUT_NAME layout.kdl
+  else if git rev-parse --is-inside-work-tree &>/dev/null
+    set NAME (basename (git rev-parse --show-toplevel))
+    set LAYOUT_NAME compact
+  end
+  if test -z $LAYOUT_NAME
     return 0
   end
-  set NAME (basename "$PWD")
+
   for EXISTING in (zellij action query-tab-names)
     if [ "$EXISTING" = "$NAME" ]
       zellij action go-to-tab-name "$NAME"
       return 1
     end
   end
-  zellij action new-tab -l layout.kdl -n "$NAME"
+  zellij action new-tab -l $LAYOUT_NAME -n "$NAME"
   return 2
 end
-
