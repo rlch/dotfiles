793696d035d4681552c29c8e28af18b333df28fc
RJM <rjmbowie@gmail.com>
2025-07-03 10:01:57 +1000

    feat: Add VimBeBetter

--- config/nvim/lua/plugins/coding.lua
+++ config/nvim/lua/plugins/coding.lua
@@ -309,4 +309,12 @@ return {
       cmdline = { enabled = false },
     },
   },
+  {
+    "szymonwilczek/vim-be-better",
+    cmd = "VimBeBetter",
+    config = function()
+      -- Optional: Enable logging for debugging
+      vim.g.vim_be_better_log_file = 1
+    end,
+  },
 }
